f51d74169f11a42af828b48d545c1d0e3a75eda4
KladeRe <klaus.rehnberg@gmail.com>
2025-01-02 22:06:52 +0200

    Added dockerignore, improved documentation and added coordinates as data type

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Multibump
 A real time multiplayer game based in browser using the websocket protocol
+
+## Architecture
+
+The architecture is based on a client-server architecture, with one server connecting multiple clients using websocket connections between the server and clients
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import WebSocket, { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 5228 });
+interface Coordinates {
+  x: number;
+  y: number;
+}
 
 wss.on('connection', (ws) => {
-  ws.send('Welcome to the game');
-
   ws.on('message', (message: string) => {
-    console.log('Received message:', message);
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+    try {
+      const coordinates: Coordinates = JSON.parse(message);
+      if (typeof coordinates.x === 'number' && typeof coordinates.y === 'number') {
+        wss.clients.forEach((client) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(coordinates));
+          }
+        });
+      } else {
+        ws.send('Invalid coordinates');
       }
-    });
+    } catch (e) {
+      ws.send('Error parsing message');
+    }
   });
 });
+
