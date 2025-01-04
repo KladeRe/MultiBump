39724f4e0206f435256d65e793ed689127220a0d
KladeRe <klaus.rehnberg@gmail.com>
2025-01-04 11:08:42 +0200

    Chaged port and configured vite config for frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      clientPort: 4367,
+    },
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 4367,
+  }
 })
