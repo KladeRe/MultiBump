6a46c8a5dec8b1a63ffc73ca4cdf4a92c4483f59
KladeRe <klaus.rehnberg@gmail.com>
2025-02-05 21:55:10 +0200

    Added line for consistent styling

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './App.css'
+
 const Login = () => {
   const [roomCode, setRoomCode] = useState('');
   const navigate = useNavigate();
