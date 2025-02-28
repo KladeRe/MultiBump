9a7e861053b615814c6c1366755fa6247649ed00
KladeRe <klaus.rehnberg@gmail.com>
2025-02-28 22:44:18 +0200

    Added renderer to single player

--- client/src/Simulation.tsx
+++ client/src/Simulation.tsx
@@ -1,6 +1,45 @@
+import { useState, useRef, useEffect } from "react";
+import { PlayerInfo, Coordinates2D } from "./util/types";
+import { Controls } from "./gameLogic/controls";
+import Renderer from "./Renderer";
 const Simulation = () => {
+
+  const params = new URLSearchParams(window.location.search);
+  const roomId = params.get("room") || "1000-0600";
+  const parts = roomId.split('-');
+  const width = parseInt(window.atob(parts[0]), 10) / 4;
+  const height = parseInt(window.atob(parts[1]), 10) / 4;
+  const [playArea] = useState<Coordinates2D>({ x: width, y: height });
+  const playerRadius = 25;
+
+  const [playerPosition, setPlayerPosition] = useState<PlayerInfo>({
+    x: playArea.x / 2,
+    y: (playArea.y * 3) / 4,
+    dx: 0,
+    dy: 0,
+  });
+
+  const opponentPosition = null;
+
+  const [lineEnd, setLineEnd] = useState({
+    x: playerPosition.x,
+    y: playerPosition.y,
+  });
+
+  const initialMousePos = useRef<Coordinates2D>({ x: 0, y: 0 });
+  const isDragging = useRef<boolean>(false);
+
+  useEffect(() => {
+      const controls = new Controls(playerPosition, playerRadius, isDragging, setLineEnd, setPlayerPosition, initialMousePos);
+      controls.addListeners();
+
+      return () => {
+        controls.removeListeners();
+      };
+  }, [playerPosition]);
+
   return (
-    <h1>Welcome to single player</h1>
+    <Renderer roomId={roomId} playArea={playArea} isDragging={isDragging} playerPosition={playerPosition} lineEnd={lineEnd} playerRadius={playerRadius} opponentPosition={opponentPosition}/>
   )
 };
 
