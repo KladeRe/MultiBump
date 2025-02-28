76427b9c7636d141c1952b6e7952d1a10bab97f1
KladeRe <klaus.rehnberg@gmail.com>
2025-02-28 17:04:56 +0200

    Created spinner for loading screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Game from './Game';
 import Login from './Login';
 import RoomMaker from './RoomMaker';
 import RoomFull from './RoomFull';
+import Spinner from './Spinner';
 const App = () => {
   return (
     <Routes>
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/login" element={<Login />}/>
         <Route path="/fullRoom" element={<RoomFull />}/>
         <Route path="/create" element={<RoomMaker />}/>
+        <Route path="/spinner" element={<Spinner />}/>
         <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
