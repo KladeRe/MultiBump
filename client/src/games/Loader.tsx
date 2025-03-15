93a361830b2b87c4e6785eaa0aa314bc05a9a76b
KladeRe <klaus.rehnberg@gmail.com>
2025-03-15 22:41:01 +0200

    feat: add possibility to return to login screen at loader (#7)

--- client/src/games/Loader.tsx
+++ client/src/games/Loader.tsx
@@ -1,10 +1,20 @@
 import Spinner from "../util/Spinner";
+import { useNavigate } from "react-router-dom";
 
 const Loader = () => {
+  const navigate = useNavigate();
+
+  const handleLoginRedirect = () => {
+    navigate("/login");
+  };
   return (
     <div className="loader-container">
       <Spinner />
       <p>Waiting for other player</p>
+      <br />
+      <button onClick={handleLoginRedirect} className="button-primary">
+        Return to login
+      </button>
     </div>
   );
 };
