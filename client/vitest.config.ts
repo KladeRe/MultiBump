1156d181e265597e563d51ea27a16f2aed572988
KladeRe <klaus.rehnberg@gmail.com>
2025-03-30 20:46:37 +0300

    Modified vitest config

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: './setupTests.ts',
+    setupFiles: "./setupTests.ts",
+    include: ["**/*.{test,spec}.{js,ts,jsx,tsx}"],
+    exclude: ["node_modules", "dist", ".git", ".cache", ".github"],
   },
 });
